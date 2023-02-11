8c13c2e596f2c9fd7518601255dca3351080a617
Yiping Lin <li1234355@gmail.com>
2023-02-11 08:09:40 +0100

    Add components to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Modal from './components/Modal';
+import Pagination from './components/Pagination';
+import Table from './components/Table';
+import logo from './logo.svg';
+import { TableBody, TableHead } from './types';
+import { getSurveyQuestions, getSurveyResults } from './utils/api';
+import {
+  surveyQuestionsTransformer,
+  surveyResultsTransformer
+} from './utils/dataTransformer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [questions, setQuestions] = useState<TableHead[] | null>(null);
+  const [results, setResults] = useState<TableBody[] | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [modalId, setModalId] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    (async () => {
+      const [q, r] = await Promise.all([
+        getSurveyQuestions(),
+        getSurveyResults()
+      ]);
+      setQuestions(surveyQuestionsTransformer(q));
+      setResults(surveyResultsTransformer(r));
+    })();
+  }, []);
+
+  const handleModalDisplay = (isVisible: boolean, dataId?: string): void => {
+    setShowModal(isVisible);
+    setModalId(dataId);
+  };
+
+  const handlePagination = (newPage: number): void => {
+    setCurrentPage(newPage);
+  }
+
+  return questions && results ? (
+    <div className="App container-fluid">
+      <Pagination
+        currentPage={currentPage}
+        totalPage={10}
+        onClick={handlePagination} />
+      <div style={{overflowX: 'auto', width: '100%', height: 'calc(100vh - 1.5rem)'}}>
+        <Table
+          head={questions}
+          body={results.slice(10*currentPage-10, 10*currentPage)}
+          handleModalDisplay={handleModalDisplay} />
+      </div>
+      {modalId && (
+        <Modal
+          id="modal"
+          question={questions}
+          result={results.filter(r => r.id === modalId)[0]}
+          visible={showModal}
+          handleVisibility={handleModalDisplay} />
+      )}
     </div>
-  );
+  ) : <div></div>;
 }
 
 export default App;
