c28c4a893d7e1018c6dd30c8e5ec714600807208
Yiping Lin <li1234355@gmail.com>
2023-02-16 19:00:54 +0100

    Add css to Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { TableBody, TableHead } from "../../types";
+import Button from "../Button";
 
 interface StyledModalProps {
   readonly visible: boolean;
@@ -22,11 +23,35 @@ const StyledModal = styled.div.attrs<StyledModalProps>(props => ({
 `;
 
 const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: #fff;
   margin: 1.5rem auto;
   width: 75%;
   border: 1px solid rgba(0, 0, 0, 0.1);
   border-radius: 0.5rem;
+`;
+
+const ModalHead = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const ModalTitle = styled.h1`
+  font-size: 1.5rem;
+  margin: 0;
+`;
+
+const ModalBody = styled.div`
+  padding: 1rem;
+
+  & h2 {
+    margin: 0;
+    font-size: 1.25rem;
+  }
 `;
 
 export interface ModalProps {
@@ -59,14 +84,20 @@ export default function Modal({
   return (
     <StyledModal id={id} visible={visible} onClick={handleClose}>
       <ModalContent onClick={e => e.stopPropagation()}>
-        <div className="modal-head">
-          <h1 className="modal-title">Modal Title</h1>
-          <button className="close" onClick={handleClose}>&times;</button>
-        </div>
-        <div className="modal-body">
-          <h2>What is the first question?</h2>
-          <p>The first answer.</p>
-        </div>
+        <ModalHead>
+          <ModalTitle>Modal Title</ModalTitle>
+          <Button theme="close" onClick={handleClose}>&times;</Button>
+        </ModalHead>
+        <ModalBody>
+          {question.map((q, id) => {
+            return (
+              <div key={id}>
+                <h2>{q.name}</h2>
+                <p>{result.content[id].data}</p>
+              </div>
+            );
+          })}
+        </ModalBody>
       </ModalContent>
     </StyledModal>
   );
